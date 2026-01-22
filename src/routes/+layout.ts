39eda7fd60ed675e4c143915f0c362f9bcbcfdfa
raphplt <raphael.plassart@gmail.com>
2026-01-22 14:03:34 +0100

    Clean project

--- src/routes/+layout.ts
+++
@@ -1,8 +0,0 @@
-import type { LayoutLoad } from './$types';
-import { waitLocale } from 'svelte-i18n';
-import '$lib/i18n'; // Import to register locales
-
-export const load: LayoutLoad = async () => {
-	await waitLocale();
-	return {};
-};
--- src/routes/docs/[slug]/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import { page } from '$app/state';
-</script>
-
-<div class="container py-24 px-4 sm:px-8 prose dark:prose-invert">
-    <h1>Documentation: {page.params.slug}</h1>
-    <p>This is a placeholder for the documentation page content.</p>
-</div>
